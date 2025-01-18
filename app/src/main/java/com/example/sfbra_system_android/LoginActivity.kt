9dc397b087294f0b8e8d9d242de08e3b3128a71e
wschoi <pk72651@gmail.com>
2025-01-18 23:05:59 +0900

    feat: 임시 로그인 화면 구성

---
+++ app/src/main/java/com/example/sfbra_system_android/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.example.sfbra_system_android
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sfbra_system_android.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 로그인 버튼 클릭 리스너
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()  // 로그인 액티비티 종료
+        }
+
+        // API 레벨 26 이상에서만 실행
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // 시스템 내비게이션 바 버튼 색상 변경
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        }
+    }
+}
